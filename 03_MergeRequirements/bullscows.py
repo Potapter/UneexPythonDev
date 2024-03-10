a8cf04c01582aa26c59b0619495e32372d6f381a
Potapter <potapter@mail.ru>
2024-03-10 11:53:28 +0300

    Workable game

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,5 @@
+import random
+
 def bullscows(guess: str, secret: str) -> (int, int):
 	bulls = set()
 	cows = set()
@@ -12,20 +14,33 @@ def bullscows(guess: str, secret: str) -> (int, int):
 	return len(bulls), len(cows)
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
-	pass
-	return
+	wished = random.choice(words)
+	
+	attempts = 0
+	while (assumption := ask("Введите слово: ", words)) != wished:
+		attempts += 1
+		inform("Быки: {}, Коровы: {}", *bullscows(assumption, wished))
+
+	return attempts
 
 def ask(prompt: str, valid: list[str] = None) -> str:
-	pass
-	return
+	print(prompt)
+	
+	if valid != None:
+		while (word := input()) not in valid:
+			print(prompt)
+	else:
+		word = input() 
+	
+	return word
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
-	pass
+	print(format_string.format(bulls, cows))
 	return
 
 
 def main():
-	print(bullscows('ропот', 'полип'))
+	print(gameplay(ask, inform, ['abc', 'abb', 'aaa']))
 	return
 	
 if __name__ == '__main__':
