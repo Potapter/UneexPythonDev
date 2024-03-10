df9496b3dcc54fcad0e33c3eeb13a85f0a0c8c2f
Potapter <potapter@mail.ru>
2024-03-10 11:26:30 +0300

    Game template

---
+++ 03_MergeRequirements/bullscows.py
@@ -0,0 +1,23 @@
+def bullscows(guess: str, secret: str) -> (int, int):
+	pass
+	return
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+	pass
+	return
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+	pass
+	return
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+	pass
+	return
+
+
+def main():
+	pass
+	return
+	
+if __name__ == '__main__':
+	main()
