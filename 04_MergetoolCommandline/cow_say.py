45b12fbb38ca008fbe2eb4970361b9bc4898f06f
Potapter <potapter@mail.ru>
2024-03-13 19:47:03 +0300

    Help description added

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -8,18 +8,22 @@ class numbername(cmd.Cmd):
     prompt = "cmd>> "
 
     def do_cowsay(self, line):
+        """Draws a cow saying a message. Required parameter: message, optional positioned pparameters: cow, eyes, tongue."""
         args = shlex.split(line)
         args = {'message': args[0], 'cow': 'default' if len(args) < 2 else args[1],
                     'eyes': 'oo' if len(args) < 3 else args[2], 'tongue': '  ' if len(args) < 4 else args[3]}
         print(cowsay.cowsay(**args))
 
     def do_list_cows(self, line):
+        """Prints a list of cows in a directory. Required parameter: directory."""
         print(cowsay.list_cows(line))
     
     def do_make_bubble(self, line):
+        """Draws a bubble with text. Required parameter: message."""
         print(cowsay.make_bubble(line))
     
     def do_cowthink(self, line):
+        """Draws a cow thinking about a message. Required parameter: message, optional positioned pparameters: cow, eyes, tongue."""
         args = shlex.split(line)
         args = {'message': args[0], 'cow': 'default' if len(args) < 2 else args[1],
                     'eyes': 'oo' if len(args) < 3 else args[2], 'tongue': '  ' if len(args) < 4 else args[3]}
