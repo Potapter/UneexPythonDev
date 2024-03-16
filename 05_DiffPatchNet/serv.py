215275ec5aca2f65a223da818621d6a66b1a6dca
Potapter <potapter@mail.ru>
2024-03-16 19:43:25 +0300

    Realised some of commands

--- 05_DiffPatchNet/serv.py
+++ 05_DiffPatchNet/serv.py
@@ -1,13 +1,12 @@
 import asyncio
 
-COMMANDS = ['who', 'cows', 'login', 'say', 'yeild', 'quit']
+COMMANDS = ['who', 'cows', 'login', 'say', 'yield', 'quit']
 
 clients = {}
 nicknames = {}
 
 async def chat(reader, writer):
     me = "{}:{}".format(*writer.get_extra_info('peername'))
-    logged_in = False
     print(me)
     clients[me] = asyncio.Queue()
     send = asyncio.create_task(reader.readline())
@@ -17,25 +16,44 @@ async def chat(reader, writer):
         for q in done:
             if q is send:
                 send = asyncio.create_task(reader.readline())
-                msng = q.result().decode().strip()
+                msng = q.result().decode().strip() + ' '
                 if msng.split()[0] not in COMMANDS:
                     await clients[me].put("SERVER: UNKNOWN COMMAND")
                     continue
-                if not logged_in and msng.split()[0] not in ['login', 'cows']:
-                    await clients[me].put("SERVER: LOGIN FIRSTLY")
-                    continue
-                else:
-                    if msng[msng.index(' ') + 1:] not in nicknames.values():
-                        nicknames[me] = msng[msng.index(' ') + 1:]
-                        logged_in = True
-                        await clients[me].put("SERVER: LOGIN SUCCESSFULLY")
-                        continue
+                if me not in nicknames.keys():
+                    if msng.split()[0] not in ['login', 'cows']:
+                        await clients[me].put("SERVER: LOGIN FIRSTLY")
+                    elif msng.split()[0] == 'login':
+                        if msng[msng.index(' ') + 1:-1] not in nicknames.values():
+                            if ' ' not in msng[msng.index(' ') + 1:-1]:
+                                nicknames[me] = msng[msng.index(' ') + 1:-1]
+                                await clients[me].put("SERVER: LOGIN SUCCESSFULLY")
+                            else:
+                                await clients[me].put("SERVER: LOGIN ERROR, NICKNAME SHOULDN'T CONTAIN SPACE")
+                        else:
+                            await clients[me].put("SERVER: NICKNAME ALREADY USED")
                     else:
-                        await clients[me].put("SERVER: NICKNAME ALREADY USED")
-                        continue
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {msng[msng.index(' ') + 1:]}")
+                        await clients[me].put("SOME NAMES")
+                else:
+                    match msng.split()[0]:
+                        case "who":
+                            await clients[me].put(nicknames.values())
+                        case "cows":
+                            await clients[me].put("SOME NAMES")
+                        case "login":
+                            await clients[me].put("ALREADY LOGGED IN")
+                        case "say":
+                            if len(msng.split()) > 2:
+                                if msng.split()[1] in nicknames.values():
+                                    await clients[list(nicknames.keys())[list(nicknames.values()).index(msng.split()[1])]].put(f"{nicknames[me]} -> {msng.split()[1]} : {' '.join(msng.split()[2:])}")
+                                else:
+                                    await clients[me].put("SERVER: RECEPIENT IS NOT ONLINE")
+                            else:
+                                await clients[me].put("SERVER: CHOOSE RECEPIENT")
+                        case "yield":
+                            for out in clients.values():
+                                if out is not clients[me]:
+                                    await out.put(f"{nicknames[me]} -> *ALL* : {' '.join(msng.split()[1:])}")
             elif q is receive:
                 receive = asyncio.create_task(clients[me].get())
                 writer.write(f"{q.result()}\n".encode())
